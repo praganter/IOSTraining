c1af8c71f634f3aacd1f218402b68e9d391d8133
Batuhan Yetgin <byetgin@engineer.com>
2021-07-23 01:48:39 +0300

    first day

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+// 1. Day of 100 days of swift
+// 1- Variables  creating with var keyword value is changeable
+// 2- Strings and Integers
+// 3- Multi-line strings
+var greeting = """
+    Hello
+    playground
+    .
+    """
+var greetings = """
+    Hello \
+    playground\
+    .
+    """
+// 4- Doubles and booleans
+// 5- String interpolation
+var str = "Batuhan"
+print("My name is \(str)")
+// 6- constants
+let pi = 3.14
+// 7-type annotations
+var str1 : String = "Batuhan"
+var integer1 : Int = 25
+var double1 : Double = 25_123
+
+// END OF DAY 1

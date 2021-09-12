cf6e7089af97c6226f63521577b830d4f89ece04
Batuhan <byetgin@engineer.com>
2021-09-12 19:15:31 +0300

    Guess The Flag

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Batuhan Yetgin on 1.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
